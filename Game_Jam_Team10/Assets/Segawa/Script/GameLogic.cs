00d71f7da9b59213e0893919addbfa89dbd66196
Masatomo Segawa <masa-segawa@masa-segawa.com>
2016-01-29 21:29:26 +0900

    Questを仮実装

--- Game_Jam_Team10/Assets/Segawa/Script/GameLogic.cs
+++ Game_Jam_Team10/Assets/Segawa/Script/GameLogic.cs
@@ -6,12 +6,19 @@ using System.Collections;
 /// </summary>
 public class GameLogic : MonoBehaviour {
 
+	// タイマーのスクリプト.
 	private Timer GameTimer;
+
+	// クエスト予告のスクリプト.
+	private Quest quest;
 
 	void Start(){
 
 		// Timer取得.
 		GameTimer = GameObject.FindGameObjectWithTag ("Timer").GetComponent<Timer>();
+
+		// Quest取得.
+		quest = GameObject.FindGameObjectWithTag ("Quest").GetComponent<Quest> ();
 
 		#region イベント登録.
 
@@ -20,8 +27,21 @@ public class GameLogic : MonoBehaviour {
 		#endregion
 	}
 
+
+	#region イベント
+	/// <summary>
+	/// プレイヤーが死んだ時に呼び出される.
+	/// </summary>
+	void E_PlayerDeath(){
+
+	}
+
+	/// <summary>
+	/// タイムオーバーした時に呼び出される.
+	/// </summary>
 	void E_TimeOver(){
 		Debug.Log ("Time Over!");
 	}
+	#endregion
 
 }
---
+++ Game_Jam_Team10/Assets/Segawa/Script/Quest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Quest : MonoBehaviour {
+
+	// 表示用のテキスト.
+	private Text textGUI;
+
+	void Start(){
+
+		textGUI = transform.GetComponentInChildren<Text> ();
+
+	}
+
+	public void SetQuest(string questTitle){
+
+		textGUI.text = questTitle;
+
+	}
+
+	/*
+	IEnumerator StartQuest(){
+
+
+
+	}*/
+
+}
