35a9b40234b42769c2d7d353c8c12a547a06349d
Masatomo Segawa <masa-segawa@masa-segawa.com>
2016-01-30 01:54:21 +0900

    EnemyのAIを仮実装(攻撃モーションとかどうしよう・・？)

---
+++ Game_Jam_Team10/Assets/Segawa/Script/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	private Animator animator;
+
+	public GameObject target;
+
+	void Start(){
+
+		animator = GetComponent<Animator> ();
+
+		target = GameObject.FindGameObjectWithTag ("Player");
+
+	}
+
+	void Update(){
+
+		// プレイヤーとの距離を取得する.
+		float distance = Vector3.Distance (transform.position, target.transform.position);
+
+		// メカニムに距離を送信.
+		animator.SetFloat("PlayerDistance", distance);
+
+	}
+		
+}
