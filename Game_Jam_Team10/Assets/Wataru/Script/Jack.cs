164c4d9e8c38eb2550999d0fe24a8981e77fd8d0
m.roppy <roppy@hotmail.co.jp>
2016-01-30 20:39:20 +0900

    かぼちゃ破壊時のエフェクト追加 背景画像一部修正

--- Game_Jam_Team10/Assets/Wataru/Script/Jack.cs
+++ Game_Jam_Team10/Assets/Wataru/Script/Jack.cs
@@ -6,6 +6,11 @@ public class Jack : MonoBehaviour {
 	private float life_dur = 3f;
 	private float life_timer = 0f;
 
+
+	[SerializeField]
+	private GameObject effectPrefab;
+	private GameObject effect;
+
 	// Use this for initialization
 	void Start () {
 	life_timer = life_dur;
@@ -15,7 +20,29 @@ public class Jack : MonoBehaviour {
 	void Update () {
 	life_timer -= Time.deltaTime;
 	if(life_timer <= 0){
-	Destroy(this.gameObject);
+	 Destroy(this.gameObject);
 	}
+	}
+
+	private void Collapse(){
+	Debug.Log( "Collapse" );
+	 effect = Instantiate(effectPrefab);
+	 effect.transform.position = transform.position;
+	 GetComponent<Collider>().enabled = false;
+	 transform.GetChild(0).gameObject.SetActive(false);
+
+	 Destroy(this.gameObject, 1f);
+	}
+
+	private void OnCollisionEnter(Collision col){
+	 if(col.gameObject.tag.Equals("Enemy")){
+		Collapse();
+     }
+	}
+
+	private void OnDestroy(){
+	 if(effect != null){
+	  Destroy(effect);
+	 }
 	}
 }
