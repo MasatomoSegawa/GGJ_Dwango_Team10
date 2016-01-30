7dba94783e4ce4f67b528c8ebfbdf6aacc280ce0
m.roppy <roppy@hotmail.co.jp>
2016-01-30 09:07:22 +0900

    仮背景追加 仮キャラ画像追加 ボタンサイズを調整 キーボード入力（デバッグ用）に対応

---
+++ Game_Jam_Team10/Assets/Wataru/Script/PlayerCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+[SerializeField]
+private Transform target;
+
+private Vector3 offset = Vector3.back * 10f;
+	
+	// Update is called once per frame
+	void Update () {
+	this.transform.localPosition = target.localPosition + offset;
+	}
+}
