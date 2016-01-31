4cf24d508d54aaad07e58f3bb28ffca408625af6
Masatomo Segawa <masa-segawa@masa-segawa.com>
2016-01-31 12:20:14 +0900

    EndingScene実装

---
+++ Game_Jam_Team10/Assets/Segawa/Script/EndingLogic.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndingLogic : MonoBehaviour {
+
+	public void OnTitleButton(){
+		FadeManager.Instance.LoadLevel ("Title",2.0f);
+	}
+
+}
